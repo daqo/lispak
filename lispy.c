ed79b023703f13ffc6afba1ef247da25752bdf5a
David Qorashi <davoud.qorashi@gmail.com>
2015-11-06 00:15:17 -0800

    Second version of the REPL

--- lispy.c
+++ lispy.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
+   
+  /* Print Version and Exit Information */
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
+   
+  /* In a never ending loop */
+  while (1) {
+    
+    /* Output our prompt and get input */
+    char* input = readline("lispy> ");
+    
+    /* Add input to history */
+    add_history(input);
+    
+    /* Echo input back to user */    
+    printf("No you're a %s\n", input);
 
-  while(1) {
-    fputs("lispy> ", stdout);
-    fgets(input, 2048, stdin);
-    printf("No you're a %s", input);
+    /* Free retrieved input */
+    free(input);
+    
   }
-
+  
   return 0;
 }
