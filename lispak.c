14f648cf5f79f900a97a6247d79772e00165b23b
David Qorashi <davoud.qorashi@gmail.com>
2015-12-18 21:48:44 -0800

    Define new functions, edit existing functions to make them compatible with s-expressions. Make lval_err and lval_num to return a LVAL*; define lval_sym, lval_sexpr and lval_del

--- lispak.c
+++ lispak.c
@@ -46,20 +46,63 @@ enum lval_type { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
 /* Create Enumeration of Possible Error Types */
 enum lval_errors { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM, LERR_OPERANDs_INVALID };
 
-/* Create a new number type lval */
-lval lval_num(Number x) {
-  lval v;
-  v.type = LVAL_NUM;
-  v.num = x;
+/* Construct a pointer to a new Number lval */ 
+lval* lval_num(Number x) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_NUM;
+  v->num = x;
   return v;
 }
 
-/* Create a new error type lval */
-lval lval_err(int x) {
-  lval v;
-  v.type = LVAL_ERR;
-  v.err = x;
+/* Construct a pointer to a new Error lval */ 
+lval* lval_err(char* m) {
+  lva*l v = malloc(sizeof(lval));
+  v->type = LVAL_ERR;
+  v->err = malloc(strlen(m) + 1);
+  strcpy(v->err, m);
   return v;
+}
+
+/* Construct a pointer to a new Symbol lval */ 
+lval* lval_sym(char* s) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_SYM;
+  v->sym = malloc(strlen(s) + 1);
+  strcpy(v->sym, s);
+  return v;
+}
+
+/* A pointer to a new empty Sexpr lval */
+lval* lval_sexpr(void) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_SEXPR;
+  v->count = 0;
+  v->cell = NULL;
+  return v;
+}
+
+void lval_del(lval* v) {
+
+  switch (v->type) {
+    /* Do nothing special for number type */
+    case LVAL_NUM: break;
+
+    /* For Err or Sym free the string data */
+    case LVAL_ERR: free(v->err); break;
+    case LVAL_SYM: free(v->sym); break;
+
+    /* If Sexpr then delete all elements inside */
+    case LVAL_SEXPR:
+      for (int i = 0; i < v->count; i++) {
+        lval_del(v->cell[i]);
+      }
+      /* Also free the memory allocated to contain the pointers */
+      free(v->cell);
+    break;
+  }
+
+  /* Free the memory allocated for the "lval" struct itself */
+  free(v);
 }
 
 /* Print an "lval" */
